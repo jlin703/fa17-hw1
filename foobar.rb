dd31f147cfcafb1a688dcd90529b7aff0ff0fa97
jlin703 <jonathan.lin.703@gmail.com>
2017-09-14 00:17:32 -0700

    Finished hw1

--- foobar.rb
+++ foobar.rb
@@ -3,7 +3,10 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    a = a.map{|item| item.to_i + 2}.uniq
+    a = a.delete_if{|item| item > 10}.select{|item| item % 2 == 0}
+    sum = 0
+    a.each{|item| sum += item}
+    return sum
   end
 end
-
-
